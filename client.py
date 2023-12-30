b8144fc2ba524ad93ee4a3b5794672b0421f0eda
sammanpathak <sammanpathak100@gmail.com>
2023-12-30 22:37:47 +0545

    updated works on my machine one

--- client.py
+++ client.py
@@ -1,148 +1,156 @@
 import socket
 import threading
-import tkinter  #it is the GUI of the python, it's part of core python stack
+import tkinter  # it is the GUI of the python, it's part of core python stack
 import tkinter.scrolledtext
 from tkinter import simpledialog
 
-HOST = '127.0.0.1' #if it is hosted then need to specify the public IP
+HOST = '127.0.0.1'  # if it is hosted then need to specify the public IP
 PORT = 9090
 
-#Client is going to be a class
+# Client is going to be a class
 # we need objects
 
+
 class Client:
-    #constructing a default constructor
+    # constructing a default constructor
     def __init__(self, host, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
-        #need a window for the message
-        msg = tkinter.Tk()  #makes the message box pop up
-        msg.withdraw() #for creating a context
+        # need a window for the message
+        msg = tkinter.Tk()  # makes the message box pop up
+        msg.withdraw()  # for creating a context
 
-        #ask for nickname in a dialog box
-        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg )
+        # ask for nickname in a dialog box
+        self.nickname = simpledialog.askstring(
+            "Nickname", "Please choose a nickname", parent=msg)
 
-        #setting a flag to manipulate the GUI
+        # setting a flag to manipulate the GUI
         self.gui_done = False
 
-        #flag if everything is running
+        # flag if everything is running
         self.running = True
 
-        #Running the threads, one for the GUI and the other for dealing with the server
-        gui_thread = threading.Thread(target = self.gui_loop)
-        receive_thread = threading.Thread(target = self.receive)
+        # Running the threads, one for the GUI and the other for dealing with the server
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive)
 
         gui_thread.start()
         receive_thread.start()
-    
-    #this is going to create the frontend
-    #creating the buttons and other stuff
+
+    # this is going to create the frontend
+    # creating the buttons and other stuff
     def gui_loop(self):
-        self.win = tkinter.Tk() #returns a toplevel widget
+        self.win = tkinter.Tk()  # returns a toplevel widget
         self.win.configure(bg="lightgray")
 
-        #setting the label to have a lightgray background, otherwise it will be white
-        self.chat_label = tkinter.Label(self.win, text="Chat:", bg="lightgray") #individual component
+        # setting the label to have a lightgray background, otherwise it will be white
+        self.chat_label = tkinter.Label(
+            self.win, text="Chat:", bg="lightgray")  # individual component
         self.chat_label.config(font=("Arial", 12))
         self.chat_label.pack(padx=20, pady=5)
 
-        #we want to have the text area now to see the chat history
-        #self.win defines that the widget is to be used
+        # we want to have the text area now to see the chat history
+        # self.win defines that the widget is to be used
         self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
         self.text_area.pack(padx=20, pady=5)
-        #Defining that the messages in the history area can't be edited
-        self.text_area.config(state='disabled') #consequence: can't alter the stylings of the messages in the history
-        #need to make it default(enable) again to style it
+        # Defining that the messages in the history area can't be edited
+        # consequence: can't alter the stylings of the messages in the history
+        self.text_area.config(state='disabled')
+        # need to make it default(enable) again to style it
 
-        #Label for message area
-        self.msg_label = tkinter.Label(self.win, text="Message:", bg="lightgray") #individual component
+        # Label for message area
+        self.msg_label = tkinter.Label(
+            self.win, text="Message:", bg="lightgray")  # individual component
         self.msg_label.config(font=("Arial", 12))
         self.msg_label.pack(padx=20, pady=5)
 
-        #Area for the message
+        # Area for the message
         self.input_area = tkinter.Text(self.win, height=3)
         self.input_area.pack(padx=20, pady=5)
 
-        #For the send button
-        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+        # For the send button
+        self.send_button = tkinter.Button(
+            self.win, text="Send", command=self.write)
         self.send_button.config(font=("Arial", 12))
         self.send_button.pack(padx=20, pady=5)
 
-        #setting the flag of the GUI to done
+        # setting the flag of the GUI to done
         self.gui_done = True
 
-        #To define what to do after we close the  window
+        # To define what to do after we close the  window
         self.win.protocol("WM_DELETE_WINDOW", self.stop)
 
-        self.win.mainloop() #calls the mainloop of Tk
+        self.win.mainloop()  # calls the mainloop of Tk
 
-    #write function defined
+    # write function defined
     def write(self):
-        #we can set the nickname from the client side as well as server side
-        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}" #1.0 meaning starting from the beginning of the text to the end
+        # we can set the nickname from the client side as well as server side
+        # 1.0 meaning starting from the beginning of the text to the end
+        message = self.input_area.get('1.0', 'end')
 
-        #send the message through the socket
+        # send the message through the socket
         self.sock.send(message.encode('utf-8'))
 
-        #After entering need to clear the text box
+        # After entering need to clear the text box
         self.input_area.delete('1.0', 'end')
 
-    #function for stopping(what to do when widget is closed)
+    # function for stopping(what to do when widget is closed)
     def stop(self):
-        #set running flag to false
+        # set running flag to false
         self.running = False
 
-        #destroy the tkinter widget after the main loop updates
+        # destroy the tkinter widget after the main loop updates
         self.win.after(1, self.win.destroy)
 
-        #close the socket
+        # close the socket
         self.sock.close()
 
-        #exit
+        # exit
         exit(0)
-    #Function for dealing with the server
+    # Function for dealing with the server
+
     def receive(self):
-        
-        #while the gui is running
+
+        # while the gui is running
         while self.running:
             try:
-                #receive a message from the server via socket
+                # receive a message from the server via socket
                 message = self.sock.recv(1024).decode('utf-8')
 
-                #if the server is asking for nickname
+                # if the server is asking for nickname
                 if message == "Nickname: ":
                     self.sock.send(self.nickname.encode('utf-8'))
-                
-                #if it isn't asking for nickname
+
+                # if it isn't asking for nickname
                 else:
 
-                    #if gui is all loaded
+                    # if gui is all loaded
                     if self.gui_done:
-                        #change the state of the text_area to normal
+                        # change the state of the text_area to normal
                         self.text_area.config(state='normal')
 
-                        #insert the message that the client has typed at the end
-                        self.text_area.insert('end', message)
+                        # insert the message that the client has typed at the end
+                        self.text_area.insert(
+                            'end', message)
 
-                        #Always scroll down to the end
+                        # Always scroll down to the end
                         self.text_area.yview('end')
-                        
-                        #Need to disable the text_area again
+
+                        # Need to disable the text_area again
                         self.text_area.config(state='disabled')
 
             except ConnectionAbortedError:
-                #if connection is aborted
+                # if connection is aborted
                 break
             except:
-                #any other type of error
+                # any other type of error
                 print("Error")
 
-                #close the socket
+                # close the socket
                 self.sock.close()
 
                 break
 
-#making an object
+
+# making an object
 client = Client(HOST, PORT)
-
-
--- server.py
+++ server.py
@@ -1,82 +1,94 @@
-#For the server we do not need GUI
-#Server only accepts the connection
-#It just broadcasts messages
+# For the server we do not need GUI
+# Server only accepts the connection
+# It just broadcasts messages
 
 import socket
 import threading
 
-HOST = '127.0.0.1' #for using it over the internet, specify your private IP(from ipconffig or ifconfig)
-#For connecting from a user you need your public IP
-#can find it in myip.is
+# for using it over the internet, specify your private IP(from ipconffig or ifconfig)
+HOST = '127.0.0.1'
+# For connecting from a user you need your public IP
+# can find it in myip.is
 PORT = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT)) #we need a tuple here
+server.bind((HOST, PORT))  # we need a tuple here
 
 server.listen()
 
-clients = []    #empty list of clients at first
-nicknames = []  #nicknames for client to chat
+clients = []  # empty list of clients at first
+nicknames = []  # nicknames for client to chat
 
-#we need to run three different functions in parallel(multithreading)
+# we need to run three different functions in parallel(multithreading)
 
-#The first one broadcasts the messages to all the connected users
+# The first one broadcasts the messages to all the connected users
+
+
 def broadcast(message):
     for client in clients:
-        client.send(message)
+        client.send(message.encode('utf-8'))
 
-#Another function will define how to handle the client once connected
+# Another function will define how to handle the client once connected
+
+
 def handle(client):
     while True:
         try:
-            #If the client is connected then 
+            # If the client is connected then
             message = client.recv(1024).decode('utf-8')
-            #since the nicknames and clients are on the same index in their respective tuples
+            # since the nicknames and clients are on the same index in their respective tuples
             print(f"{nicknames[clients.index(client)]} says {message}")
+            broadcast(f'{nicknames[clients.index(client)]}: {message}')
         except:
-            #If the client gets disconnected or any error occurs
+            # If the client gets disconnected or any error occurs
             index = clients.index(client)
-            #remove the client
+            # remove the client
             clients.remove(client)
-            #close the connection from the client
+            # close the connection from the client
             client.close()
 
-            #now remove the nickname
+            # now remove the nickname
             nickname = nicknames[index]
             print(f"{nickname} has left the chat!")
             nicknames.remove(nickname)
             break
-            
 
-#Another function would continuously listen for new connections 
-#It would receive connections
-#It has to run on the main thread as once receiving connections is stopped, other threads should also be stopped
+
+# Another function would continuously listen for new connections
+# It would receive connections
+# It has to run on the main thread as once receiving connections is stopped, other threads should also be stopped
 def receive():
     while True:
-        client, address = server.accept()   #The server has to accept new connections continuously
-        #we establish a socket with the client
-        #The client is the socket and address is address of the connection 
-        print(f"Connected with{str(address)}")  #address has to be typecasted to string
-        #whenever we send something from the server we send it to the client via the socket
+        # The server has to accept new connections continuously
+        client, address = server.accept()
+        # we establish a socket with the client
+        # The client is the socket and address is address of the connection
+        # address has to be typecasted to string
+        print(f"Connected with{str(address)}")
+        # whenever we send something from the server we send it to the client via the socket
         client.send("Nickname: ".encode('utf-8'))
-        nickname = client.recv(1024).decode('utf-8') #receiving 1024 bytes as a response from the client
-        #need to include the nickname of the client in the  nicknames tuple
+        # receiving 1024 bytes as a response from the client
+        nickname = client.recv(1024).decode('utf-8')
+        # need to include the nickname of the client in the  nicknames tuple
         nicknames.append(nickname)
-        #Also need to know the socket for the connection
+        # Also need to know the socket for the connection
         clients.append(client)
-        #notice one thing that the nickname and client end up in their respective tuple in the same position
-        #we can use index to see which nickname belongs to which client
+        # notice one thing that the nickname and client end up in their respective tuple in the same position
+        # we can use index to see which nickname belongs to which client
 
-        print(f"Nickname of the client is: {nickname}") #Displaying the nickname of the client
-        #Let others know that a person  with a specific nickname has joined the chat
-        broadcast(f"{nickname} connected to the server!\n".encode('utf-8'))
-        #Just an acknowledgement to the respective client by the server
+        # Displaying the nickname of the client
+        print(f"Nickname of the client is: {nickname}")
+        # Let others know that a person  with a specific nickname has joined the chat
+        # broadcast(f"{nickname} connected to the server!\n")
+        # Just an acknowledgement to the respective client by the server
         client.send("connected to the server.".encode('utf-8'))
 
-        #now it is time to send the client to the handle function
-        #using threading for this purpose
-        thread = threading.Thread(target=handle, args=(client,)) #The reason for comma is that it requires a tuple
+        # now it is time to send the client to the handle function
+        # using threading for this purpose
+        # The reason for comma is that it requires a tuple
+        thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
 
 print("Server running.....")
 receive()
